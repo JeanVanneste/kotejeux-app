1c6ab05da0e26c021b2887b004280f814f683837
JeanVanneste <jean.vanneste@student.vinci.be>
2019-01-03 21:45:02 +0100

    Création component jeux et editeurs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { JeuxComponent } from './jeux/jeux.component';
+import { EditeursComponent } from './editeurs/editeurs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JeuxComponent,
+    EditeursComponent
   ],
   imports: [
     BrowserModule
