5f1ca3663e0d39ca7434adbd953f4348202a4df0
JeanVanneste <jean.vanneste@student.vinci.be>
2019-01-03 22:07:06 +0100

    edition editeur component

--- src/app/editeurs/editeurs.component.ts
+++ src/app/editeurs/editeurs.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Editeur } from '../editeur';
+
 @Component({
   selector: 'app-editeurs',
   templateUrl: './editeurs.component.html',
   styleUrls: ['./editeurs.component.css']
 })
 export class EditeursComponent implements OnInit {
+
+  editeur: Editeur = {
+    id: 4,
+    name: 'Lui-même',
+    nationalite: 'France',
+    creationYear: 1999
+  }
 
   constructor() { }
 
