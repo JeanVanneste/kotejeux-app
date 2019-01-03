9681f5bcacc38dfaebfc5ea79abc0a950fa3c808
JeanVanneste <jean.vanneste@student.vinci.be>
2019-01-03 22:11:45 +0100

    edition jeu component

--- src/app/jeux/jeux.component.ts
+++ src/app/jeux/jeux.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Jeu } from '../jeu';
+
 @Component({
   selector: 'app-jeux',
   templateUrl: './jeux.component.html',
   styleUrls: ['./jeux.component.css']
 })
 export class JeuxComponent implements OnInit {
+
+  jeu: Jeu = {
+    id: 1,
+    editeurId: 4,
+    name: "Les Loups-garous de Thiercelieux",
+    author: "Phillipe des Pallières et Hervé Marly",
+    category: 'Ambiance',
+    gameDuration: 30,
+    playerMin: 6,
+    playerMax: 48,
+    description: "Les Loups-Garous de Thiercelieux est un jeu de société d'ambiance dans lequel chaque joueur incarne un villageois ou un loup-garou"
+  }
 
   constructor() { }
 
