54ed804cd44a8b7ea2b9f2fcaaf507e75c9c7b76
JeanVanneste <jean.vanneste@student.vinci.be>
2019-01-04 18:35:52 +0100

    Creation RoutingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { JeuxComponent } from './jeux/jeux.component';
 import { EditeursComponent } from './editeurs/editeurs.component';
 import { EditeurDetailComponent } from './editeur-detail/editeur-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { MessagesComponent } from './messages/messages.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
