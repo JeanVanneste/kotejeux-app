a46277828f2c77e197ff463b4c5110ec57b89a91
JeanVanneste <jean.vanneste@student.vinci.be>
2019-01-04 18:21:07 +0100

    send fetched message

--- src/app/editeur.service.ts
+++ src/app/editeur.service.ts
@@ -3,15 +3,18 @@ import { Observable, of } from 'rxjs';
 
 import { Editeur } from './editeur';
 import { EDITORS } from './editeurs-test';
+import { MessageService } from '/message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EditeurService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getEditeurs(): Observable<Editeur[]> {
+    // TODO: send message _after_ fetching the editors
+    this.messageService.add('EditeurService: fetched editeurs');
     return of(EDITORS);
   }
 }
