68382b673bbbd650dc90521713ecedbe45049e55
JeanVanneste <jean.vanneste@student.vinci.be>
2019-01-04 11:21:40 +0100

    New editeur test

--- src/app/editeurs-test.ts
+++ src/app/editeurs-test.ts
@@ -2,6 +2,7 @@ import { Editeur } from './editeur';
 
 export const EDITORS: Editeur[] = [
   { id: 1, name: "Asmodée", nationalite: "France", creationYear: 1986 },
-  { id: 2, name: "Hasbro", nationalite: "USA", creationYear: 1923}
-  { id: 3, name: "Games Workshop", nationalite: "Angleterre", creationYear: 1975}
+  { id: 2, name: "Hasbro", nationalite: "USA", creationYear: 1923 },
+  { id: 3, name: "Games Workshop", nationalite: "Angleterre", creationYear: 1975 },
+  { id: 4, name: 'Lui-même', nationalite: 'France', creationYear: 1999 }
 ]
