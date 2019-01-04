80b9ab9db5699dd8604ea7739de829223cfca609
JeanVanneste <jean.vanneste@student.vinci.be>
2019-01-04 18:05:01 +0100

    Création component messages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { JeuxComponent } from './jeux/jeux.component';
 import { EditeursComponent } from './editeurs/editeurs.component';
 import { EditeurDetailComponent } from './editeur-detail/editeur-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     JeuxComponent,
     EditeursComponent,
-    EditeurDetailComponent
+    EditeurDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
