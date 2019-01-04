c4a14de7f30f9df5cb441f53f98a2761126c8109
JeanVanneste <jean.vanneste@student.vinci.be>
2019-01-04 11:19:00 +0100

    creation editeur test

---
+++ src/app/editeurs-test.ts
@@ -0,0 +1,7 @@
+import { Editeur } from './editeur';
+
+export const EDITORS: Editeur[] = [
+  { id: 1, name: "Asmodée", nationalite: "France", creationYear: 1986 },
+  { id: 2, name: "Hasbro", nationalite: "USA", creationYear: 1923}
+  { id: 3, name: "Games Workshop", nationalite: "Angleterre", creationYear: 1975}
+]
