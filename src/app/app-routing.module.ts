a289f0e99215756efc321a8e55172c4ac997c234
JeanVanneste <jean.vanneste@student.vinci.be>
2019-01-04 22:01:47 +0100

    Rajout route vers editeur-detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { EditeursComponent } from './editeurs/editeurs.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { EditeurDetailComponent } from './editeur-detail/editeur-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch:'full' },
   { path: 'editeurs', component: EditeursComponent },
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: EditeurDetailComponent }
 ];
 
 @NgModule({
