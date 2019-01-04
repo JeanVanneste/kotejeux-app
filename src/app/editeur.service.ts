3775c6ca3568472b4b5ca50da0f8f00afb156daa
JeanVanneste <jean.vanneste@student.vinci.be>
2019-01-04 17:33:18 +0100

    Correction nom classe

--- src/app/editeur.service.ts
+++ src/app/editeur.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import { Editeur } from './editeur';
-import { EDITORS } from './editeurs-test'
+import { EDITORS } from './editeurs-test';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +10,7 @@ export class EditeurService {
 
   constructor() { }
 
-  getEditeurs(): Hero[] {
+  getEditeurs(): Editeur[] {
     return EDITORS;
   }
 }
