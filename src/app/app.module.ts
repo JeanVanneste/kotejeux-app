4342a8dfecdea99a27d1653878c7265a63907bbe
JeanVanneste <jean.vanneste@student.vinci.be>
2019-01-05 10:41:51 +0100

    Adding http request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,9 @@ import { InMemoryDataService } from './in-memory-data.service';
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
     HttpClientInMemoryWebApiModule.forRoot(
       InMemoryDataService, { dataEncapsulation: false }
     ),
--- src/app/editeur.service.ts
+++ src/app/editeur.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
 
 import { Editeur } from './editeur';
 import { EDITORS } from './editeurs-test';
@@ -9,8 +10,11 @@ import { MessageService } from './message.service';
   providedIn: 'root'
 })
 export class EditeurService {
+  private editeursUrl = 'api/editeur'; //URL to web API
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
   getEditeur(id: number): Observable<Editeur> {
     // TODO: send message _after_ fetching the editor
@@ -18,9 +22,12 @@ export class EditeurService {
     return of(EDITORS.find(editeur => editeur.id === id));
   }
 
+  private log(message: string) {
+    this.messageService.add(`EditeurService: ${message}`);
+  }
+
   getEditeurs(): Observable<Editeur[]> {
     // TODO: send message _after_ fetching the editors
-    this.messageService.add('EditeurService: fetched editeurs');
-    return of(EDITORS);
+    return this.http.get<Editeur[]>(this.editeursUrl)
   }
 }
